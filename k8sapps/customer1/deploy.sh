19c924c5dc4cea610f78408702b1f95223722d7f
amusham <amusham@AMUSHAM-IN.oradev.oraclecorp.com>
2021-07-23 18:20:54 +0530

    add files

---
+++ k8sapps/customer1/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$deploy" == "app1" ]
+then
+    cp -rf /myapps/app1/* /var/www/html/
+    httpd -DFOREGROUND
+elif [ "$deploy" == "app2" ]
+then
+    cp -rf /myapps/app2/* /var/www/html/
+    httpd -DFOREGROUND
+elif [ "$deploy" == "app3" ]
+then
+    cp -rf /myapps/app3/* /var/www/html/
+    httpd -DFOREGROUND
+else
+    echo "Hello, you need to check with Docker image builder team"
+    httpd -DFOREGROUND
+fi
